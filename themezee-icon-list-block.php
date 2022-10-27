f5603bad2beddae9fb016c41a0fefc8b65bf4f3c
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-10-27 14:18:33 +0200

    added license settings and plugin updater

--- themezee-icon-list-block.php
+++ themezee-icon-list-block.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Themezee Icon List Block
+ * Plugin Name:       ThemeZee Icon List Block
  * Description:       Display a SVG icon.
  * Requires at least: 6.0
  * Requires PHP:      7.0
@@ -25,3 +25,52 @@ function register_themezee_icon_list_block() {
 	register_block_type( __DIR__ . '/build/icon-list-item' );
 }
 add_action( 'init', 'register_themezee_icon_list_block' );
+
+
+/**
+ * Set up the Plugin Updater Constants.
+ */
+define( 'THEMEZEE_ICON_LIST_BLOCK_VERSION', '1.0' );
+define( 'THEMEZEE_ICON_LIST_BLOCK_NAME', 'Icon List Block' );
+define( 'THEMEZEE_ICON_LIST_BLOCK_ID', 53694 );
+define( 'THEMEZEE_ICON_LIST_BLOCK_STORE_URL', 'https://themezee.com' );
+
+
+/**
+ * Include License Settings and Plugin Updater.
+ */
+include dirname( __FILE__ ) . '/includes/class-themezee-blocks-admin-page.php';
+include dirname( __FILE__ ) . '/includes/class-themezee-icon-list-block-license-settings.php';
+include dirname( __FILE__ ) . '/includes/class-themezee-icon-list-block-updater.php';
+
+
+/**
+ * Initialize the updater. Hooked into `init` to work with the
+ * wp_version_check cron job, which allows auto-updates.
+ */
+function update_themezee_icon_list_block() {
+
+	// To support auto-updates, this needs to run during the wp_version_check cron job for privileged users.
+	$doing_cron = defined( 'DOING_CRON' ) && DOING_CRON;
+	if ( ! current_user_can( 'manage_options' ) && ! $doing_cron ) {
+		return;
+	}
+
+	// Retrieve our license key from the DB.
+	$options     = get_option( 'themezee_blocks_settings', array() );
+	$license_key = ! empty( $options['icon_list_block_license_key'] ) ? trim( $options['icon_list_block_license_key'] ) : false;
+
+	// Setup the updater.
+	$edd_updater = new ThemeZee_Icon_List_Block_Updater(
+		THEMEZEE_ICON_LIST_BLOCK_STORE_URL,
+		__FILE__,
+		array(
+			'version' => THEMEZEE_ICON_LIST_BLOCK_VERSION,
+			'license' => $license_key,
+			'item_id' => THEMEZEE_ICON_LIST_BLOCK_ID,
+			'author'  => 'ThemeZee',
+			'beta'    => false,
+		)
+	);
+}
+add_action( 'init', 'update_themezee_icon_list_block' );
