56e6df9dc59e5588a34653cb7bfcff818d8ddf4b
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-11-09 15:02:00 +0100

    run build

---
+++ build/icon-list-item/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '34e85c8c7643ae2df317');
---
+++ build/icon-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'b3603ed2349d31aed08f');
