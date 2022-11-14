aeae38271b4d102e7bf4a23bf74b63489ace841a
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-11-14 10:17:31 +0100

    updated plugin description

--- themezee-icon-list-block.php
+++ themezee-icon-list-block.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       ThemeZee Icon List Block
- * Description:       Display a SVG icon.
+ * Description:       Create an icon list.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           1.0
